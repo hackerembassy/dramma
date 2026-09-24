16eb7438cd23bf9cbc3a7a2979632db4256a16eb
eupn <eupn@protonmail.com>
2026-09-24 21:49:49 +0400

    style: run rustfmt

--- src/cat_fetch.rs
+++ src/cat_fetch.rs
@@ -13,7 +13,10 @@ struct CatResponseItem {
 pub async fn fetch_cat_image() -> Result<RgbImage, Box<dyn std::error::Error + Send + Sync>> {
     info!("Fetching random cat info from API...");
     let request = Request::get("https://api.thecatapi.com/v1/images/search")
-        .header("User-Agent", &format!("dramma/{}", env!("CARGO_PKG_VERSION")))
+        .header(
+            "User-Agent",
+            &format!("dramma/{}", env!("CARGO_PKG_VERSION")),
+        )
         .body(())?;
 
     let mut response = isahc::send_async(request).await?;
